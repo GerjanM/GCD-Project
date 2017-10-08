de1e8a51099564368a34d2ade1263beb41e691e6
Gerjan Maas <gerjan.maas@gmail.com>
2017-10-08 11:38:28 +0200

    final commit

--- run_analysis.R
+++ run_analysis.R
@@ -13,7 +13,7 @@ subject_train<-read.table("subject_train.txt")
 X_train<-read.table("X_train.txt")
 Y_train<-read.table("y_train.txt")
 
-##4. Appropriately labels the data set with descriptive variable names
+## Appropriately labels the data set with descriptive variable names
 names(X_test)<-features[,2]
 names(Y_test)<-"activity"
 names(subject_test)<-"person"
@@ -22,28 +22,33 @@ names(X_train)<-features[,2]
 names(Y_train)<-"activity"
 names(subject_train)<-"person"
 
-##1. Merges the training and the test sets to create one data set.
+## Merges the training and the test sets to create one data set.
 merge_test<-cbind(Y_test,subject_test,X_test)
 merge_train<-cbind(Y_train,subject_train,X_train)
 merge_total<-rbind(merge_test,merge_train)
 
-##2. Extracts only the measurements on the mean and standard deviation for each measurement
+## Extracts only the measurements on the mean and standard deviation for each measurement
 a<-(grep("std()|mean()|person|activity",names(merge_total),value = TRUE))
 merge_select<-merge_total[,a]
 
-##3. Uses descriptive activity names to name the activities in the data set
+## Uses descriptive activity names to name the activities in the data set
 merge_select$activity<-factor(merge_select$activity,labels = actLabels$V2)       
 
-##4. Appropriately labels the data set with descriptive variable names. 
-names(merge_select)<-(sub("()","",names(merge_select),fixed=TRUE))
-names(merge_select)<-gsub("Body","B",names(merge_select))
-names(merge_select)<-tolower(names(merge_select)) 
+## Appropriately labels the data set with descriptive variable names. 
+names(merge_select)<-sub("\\(\\)","",names(merge_select))
+names(merge_select)<-sub("-","",names(merge_select))
+names(merge_select)<-gsub("-mean","Mean",names(merge_select))
+names(merge_select)<-gsub("-std","Std",names(merge_select))
 
-##5. create a second data set with the average of each variable for each activity and each subject
+## Create a second data set with the average of each variable for each activity and each subject
 library(reshape2)
 tidy_mean_melt <- melt(merge_select, id = c("activity","person"))
 tidy_mean_cast <- dcast(tidy_mean_melt, activity+person ~ variable, mean)
 
-setwd(..nog vullen)
-write.table(tidy_mean_cast, "tidy.txt", row.names = FALSE, quote = FALSE)
+setwd("D:/Users/Gerjan/R/GCD-Project")
+write.table(tidy_mean_cast, "tidyGCDdata.txt", row.names = FALSE, quote = FALSE)
+
+# Generate Basic for CodeBook
+write.table(names(merge_select),"CodeBook.md",row.names = FALSE, quote = FALSE)
+write.table(actLabels,"CodeBook.md",row.names = FALSE,col.names = FALSE, quote = FALSE,append = TRUE)
 
